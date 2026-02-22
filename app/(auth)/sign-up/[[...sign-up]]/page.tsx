84734ef06148ca4e10a33248a1ee8f8d0f95cf40
Prasanjeet Panwar <ppanwar@cholachariots.com>
2026-02-22 15:59:22 +0530

    added the auth page

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,9 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs";
 
-export default function SignInPage() {
+export default function SignUpPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-[#F5F4F0]">
-      <SignIn />
+      <SignUp />
     </div>
   );
 }
